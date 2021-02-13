d734d9bd98895d16fc6abe6cda10419f38bb4040
blue_islands <h.aoshima@livlog.jp>
2021-02-14 00:09:37 +0900

    no message

--- test/jp/livlog/normalizeNumexp/normalizeNumexp/NormalizeNumexpTest.java
+++ test/jp/livlog/normalizeNumexp/normalizeNumexp/NormalizeNumexpTest.java
@@ -66,4 +66,94 @@ class NormalizeNumexpTest {
         org.junit.Assert.assertEquals("numerical*２人*8*10*人*2*2*", result.get(0));
         org.junit.Assert.assertEquals("reltime*昨年3月*0*4*none*XXXX-03-XX,P-1Y*XXXX-03-XX,P-1Y*", result.get(1));
     }
+
+
+    @Test
+    void simple4() {
+
+        final var methodName = new Object() {
+        }.getClass().getEnclosingMethod().getName();
+        final var text = "131.1ポイントというスコアを叩き出した";
+        final List <String> result = new ArrayList <>();
+        final var language = "ja";
+        this.NN = new NormalizeNumexpImpl(language);
+        this.NN.normalize(text, result);
+        for (final String line : result) {
+            System.out.println(methodName + ":" + line);
+        }
+        org.junit.Assert.assertEquals(1, result.size());
+        org.junit.Assert.assertEquals("numerical*131.1ポイント*0*9*ポイント*131.1*131.1*", result.get(0));
+    }
+
+
+    @Test
+    void simple5() {
+
+        final var methodName = new Object() {
+        }.getClass().getEnclosingMethod().getName();
+        final var text = "午後3時45分に待ち合わせ";
+        final List <String> result = new ArrayList <>();
+        final var language = "ja";
+        this.NN = new NormalizeNumexpImpl(language);
+        this.NN.normalize(text, result);
+        for (final String line : result) {
+            System.out.println(methodName + ":" + line);
+        }
+        org.junit.Assert.assertEquals(1, result.size());
+        org.junit.Assert.assertEquals("abstime*午後3時45分*0*7*none*15:45:XX*15:45:XX*", result.get(0));
+    }
+
+
+    @Test
+    void dayOfWeek1() {
+
+        final var methodName = new Object() {
+        }.getClass().getEnclosingMethod().getName();
+        final var text = "5月3日(水)";
+        final List <String> result = new ArrayList <>();
+        final var language = "ja";
+        this.NN = new NormalizeNumexpImpl(language);
+        this.NN.normalize(text, result);
+        for (final String line : result) {
+            System.out.println(methodName + ":" + line);
+        }
+        org.junit.Assert.assertEquals(1, result.size());
+        org.junit.Assert.assertEquals("abstime*5月3日(水)*0*7*none*XXXX-05-03*XXXX-05-03*Wed", result.get(0));
+    }
+
+
+    @Test
+    void dayOfWeek2() {
+
+        final var methodName = new Object() {
+        }.getClass().getEnclosingMethod().getName();
+        final var text = "2001/3/3 Sat";
+        final List <String> result = new ArrayList <>();
+        final var language = "ja";
+        this.NN = new NormalizeNumexpImpl(language);
+        this.NN.normalize(text, result);
+        for (final String line : result) {
+            System.out.println(methodName + ":" + line);
+        }
+        org.junit.Assert.assertEquals(1, result.size());
+        org.junit.Assert.assertEquals("abstime*2001/3/3 Sat*0*12*none*2001-03-03*2001-03-03*Sat", result.get(0));
+    }
+
+
+    @Test
+    void realExample1() {
+
+        final var methodName = new Object() {
+        }.getClass().getEnclosingMethod().getName();
+        final var text = "【今日から開催】The Fruits of Adventures @ ZEIT-FOTO SALON(東京・京橋)  4/26(Tue)まで";
+        final List <String> result = new ArrayList <>();
+        final var language = "ja";
+        this.NN = new NormalizeNumexpImpl(language);
+        this.NN.normalize(text, result);
+        for (final String line : result) {
+            System.out.println(methodName + ":" + line);
+        }
+        org.junit.Assert.assertEquals(1, result.size());
+        org.junit.Assert.assertEquals("abstime*4/26(Tue)まで*59*70*none*XXXX-04-26*XXXX-04-26*Tue", result.get(0));
+    }
 }
